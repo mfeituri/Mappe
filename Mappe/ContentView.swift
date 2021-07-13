c5d49ea73ac7208e791dc5a8dd37cd78759c2411
Mohamed Feituri <midofeituri@gmail.com>
2021-07-13 14:28:29 -0400

    Initial Commit

---
+++ Mappe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mappe
+//
+//  Created by Mohamed Feituri on 7/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mappe/MappeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappeApp.swift
+//  Mappe
+//
+//  Created by Mohamed Feituri on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MappeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
